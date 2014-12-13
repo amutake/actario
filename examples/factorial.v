bc301792fc7d91d06dc61354be558dcb65f87480
amutake <amutake.s@gmail.com>
2014-12-14 01:13:28 +0900

    Add properties and verify example

--- examples/factorial.v
+++ examples/factorial.v
@@ -1,7 +1,8 @@
 Set Implicit Arguments.
 Unset Strict Implicit.
 
-Require Import NPeano Actor.syntax Actor.semantics.
+Require Import List NPeano Actor.syntax Actor.semantics Actor.properties.
+Import ListNotations.
 
 (**
  * ref: An Algebraic Theory of Actors and its Actors and Application to a Simple Object-Based Language (Gul Agha, 2004)
@@ -31,26 +32,119 @@ CoFixpoint factorial_behv : behavior :=
            | _ => become factorial_behv
          end).
 
-Definition result_wait_behv (expect : nat) : behavior :=
-  beh (fun msg =>
-         match msg with
-           | nat_msg result =>
-             (* n を観測したい *)
-             if (Nat.eqb result expect) then
-               (* OK *)
-               become empty_behv
-             else
-               (* まちがってる *)
-               become empty_behv
-           | _ => become empty_behv
-         end).
-
-Definition factorial_system : config :=
+Definition factorial_system (n : nat) : config :=
   init "factorial" (
          x <- new factorial_behv;
          me <- self;
-         x ! tuple_msg (nat_msg 5) (name_msg me);
-         become (result_wait_behv (5 * 4 * 3 * 2 * 1))
+         x ! tuple_msg (nat_msg n) (name_msg me);
+         become empty_behv
        ).
 
 (* OK 状態にくることを検証したいのだけど、どうやればいいのか *)
+
+(* toplevel 使って名前を指定するところが微妙 *)
+
+Open Scope string_scope.
+
+(* (factorial_system 0) から、任意の遷移をした後、(toplevel "factorial") に向けて (nat_msg 1) というメッセージが送られる遷移が存在する *)
+Theorem deliver_1 :
+  deliver_exists (factorial_system 0) (toplevel "factorial") (nat_msg 1).
+Proof.
+  pose (top := toplevel "factorial").
+  pose (factorial := (generated top 0)).
+
+  unfold deliver_exists.
+  exists [], (become empty_behv), [], 1.
+  exists [actor_state factorial (become factorial_behv) [] 0], []. (* gen_number, actors_l, actors_r とか何でも良くない？ *)
+  simpl; split.
+  - unfold factorial_system.
+    pose (fact_actor := actor_state factorial (become factorial_behv) [] 0).
+    pose (msg0 := fun me => tuple_msg (nat_msg 0) (name_msg me)).
+    pose (conf1 := conf []
+                        [ fact_actor;
+                          actor_state top (me <- self;
+                                           factorial ! (msg0 me);
+                                           become empty_behv) [] 1
+                        ]).
+    eapply trans_trans.
+    (* 遷移を1つずつ証明しないといけなくて、かなり小規模なものでも粒度細かすぎてめんどくさい。。 *)
+    {
+      exists New.
+      assert (init "factorial" (
+                     (x) <- new factorial_behv;
+                     (me) <- self ;
+                     (x) ! msg0 me; become empty_behv
+                   ) ~(New)~> conf1).
+      {
+        unfold init.
+        apply trans_new with (actors_l := []).
+      }
+      apply H.
+    }
+    pose (conf2 := conf []
+                        [ fact_actor;
+                          actor_state top (factorial ! msg0 top;
+                                           become empty_behv) [] 1
+                        ]).
+    eapply trans_trans.
+    {
+      exists Self.
+      assert (conf1 ~(Self)~> conf2).
+      {
+        apply trans_self with (actors_l := [fact_actor]).
+      }
+      apply H.
+    }
+    pose (conf3 := conf [send_message factorial (msg0 top)]
+                        [ fact_actor;
+                          actor_state top (become empty_behv) [] 1]).
+    eapply trans_trans.
+    {
+      exists Send.
+      assert (conf2 ~(Send)~> conf3).
+      {
+        apply trans_send with (actors_l := [fact_actor]).
+      }
+      apply H.
+    }
+    pose (top_actor := actor_state top (become empty_behv) [] 1).
+    pose (conf4 := conf []
+                        [ actor_state factorial (become factorial_behv) [msg0 top] 0;
+                          top_actor]).
+    eapply trans_trans.
+    {
+      exists Deliver.
+      assert (conf3 ~(Deliver)~> conf4).
+      {
+        apply trans_deliver with (actors_l := []).
+      }
+      apply H.
+    }
+    pose (conf5 := conf []
+                        [ actor_state factorial (top ! nat_msg 1; become factorial_behv) [] 0;
+                          top_actor]).
+    eapply trans_trans.
+    {
+      exists Open.
+      assert (conf4 ~(Open)~> conf5).
+      {
+        apply trans_open with (actors_l := []).
+      }
+      apply H.
+    }
+    pose (conf6 := conf [send_message top (nat_msg 1)] [fact_actor; top_actor]).
+    eapply trans_trans.
+    {
+      exists Send.
+      assert (conf5 ~(Send)~> conf6).
+      {
+        apply trans_send with (actors_l := []).
+      }
+      apply H.
+    }
+    apply trans_refl.
+  - exists (conf []
+                 [ actor_state factorial (become factorial_behv) [] 0;
+                   actor_state top (become empty_behv) [nat_msg 1] 1]).
+    apply trans_deliver with (actors_l := [actor_state factorial (become factorial_behv) [] 0]).
+Qed.
---
+++ theories/properties.v
@@ -0,0 +1,35 @@
+Set Implicit Arguments.
+Unset Strict Implicit.
+
+Require Import List.
+Import ListNotations.
+
+Require Import syntax semantics.
+
+Definition deliver_exists (ini : config) (target : name) (msg : message) :=
+  exists global_queue actions queue number actors_l actors_r,
+    let c := (conf (send_message target msg :: global_queue)
+              (actors_l
+                 ++ actor_state target actions queue number
+                 :: actors_r)) in
+    ini ~>* c /\ (exists c', c ~(Deliver)~> c').
+
+Definition open_exists (ini : config) (target : name) (msg : message) :=
+  exists global_queue behv queue number actors_l actors_r,
+    forall c,
+    c = (conf global_queue
+              (actors_l
+                 ++ actor_state target (become behv) (msg :: queue) number
+                 :: actors_r)) ->
+    ini ~>* c /\ (exists c', c ~(Open)~> c').
+
+Definition open_exists_with_behv (ini : config) (target : name) (msg : message) (behv : behavior) :=
+  exists global_queue queue number actors_l actors_r,
+    forall c,
+    c = (conf global_queue
+              (actors_l
+                 ++ actor_state target (become behv) (msg :: queue) number
+                 :: actors_r)) ->
+    ini ~>* c /\ (exists c', c ~(Open)~> c').
+
+(* (msg : message) じゃなくて (P : msg -> Prop) とかのほうが良さそう *)
