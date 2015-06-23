b9ce53c0bf0e2e63b0611b75c51ed6452fcff809
amutake <amutake.s@gmail.com>
2015-06-23 11:17:22 +0900

    introduce perm_eq and perm_eq_actors

--- src/semantics.v
+++ src/semantics.v
@@ -1,7 +1,7 @@
 Set Implicit Arguments.
 Unset Strict Implicit.
 
-Require Import Ssreflect.eqtype Ssreflect.ssrbool Ssreflect.seq.
+Require Import Ssreflect.eqtype Ssreflect.ssrbool Ssreflect.seq Ssreflect.ssrnat.
 Require Import syntax.
 
 Section Label.
@@ -52,38 +52,79 @@ Section Label.
   Canonical label_eqType := Eval hnf in EqType label label_eqMixin.
 End Label.
 
-Reserved Notation "c1 '~(' t ')~>' c2" (at level 60).
+(* count =  *)
+(* fun (T : Type) (a : pred T) => *)
+(* fix count (s : seq T) : nat := *)
+(*   match s with *)
+(*   | [::] => 0 *)
+(*   | x :: s' => ssrnat.addn (ssrnat.nat_of_bool (a x)) (count s') *)
+(*   end *)
+(*      : forall T : Type, pred T -> seq T -> nat *)
 
-(* transition defined as a relation of two config with label *)
-(* trans label conf conf': label という遷移のラベルで conf が遷移して conf' になる *)
+(* pred1 =  *)
+(* fun (T : eqType) (a1 : T) => SimplPred (T:=T) (xpred1 a1) *)
+(*      : forall T : eqType, T -> simpl_pred T *)
+
+(* Notation xpred1 := (fun a1 x => eq_op x a1) *)
+
+(* SimplPred =  *)
+(* fun (T : Type) (p : pred T) => ssrfun.SimplFun p *)
+(*      : forall T : Type, pred T -> simpl_pred T *)
+
+(* Notation count_mem x := (count (pred1 x)) *)
+
+(* Actor version of perm_eq *)
+(* This is because actor cannot be an instance of eqType (actions cannot be an instance of eqType because actions is defined as co-inductive type) *)
+
+Notation xpred1_actor := (fun a x => actor_name a == actor_name x).
+
+Definition pred1_actor (a : actor) :=
+  SimplPred (xpred1_actor a).
+
+Notation count_mem_actor a := (count (pred1_actor a)).
+
+Fixpoint perm_eq_actors (s1 s2 : seq actor) : bool :=
+  all (SimplPred (T := actor) (fun a : actor => (count_mem_actor a) s1 == (count_mem_actor a) s2)) (s1 \cup s2).
+
+
+(* labeled transition semantics *)
+(* between two configurations with a label *)
+Reserved Notation "c1 '~(' t ')~>' c2" (at level 60).
 Inductive trans : label -> config -> config -> Prop :=
 (* receive transition *)
 | trans_receive :
-    forall sendings to from content f gen actors,
-      ([:: Build_sending to from content] \cup sendings) >< ([:: Build_actor to (become (receive f)) gen] \cup actors)
-        ~(Receive to from content)~>
-        sendings >< ([:: Build_actor to (f content) gen] \cup actors)
+    forall sendings to from content f gen actors s s' a a',
+      perm_eq s ([:: Build_sending to from content] \cup sendings) ->
+      perm_eq s' sendings ->
+      perm_eq_actors a ([:: Build_actor to (become (receive f)) gen] \cup actors) ->
+      perm_eq_actors a' ([:: Build_actor to (f content) gen] \cup actors) ->
+      (s >< a) ~(Receive to from content)~> (s' >< a')
 (* send transition *)
 | trans_send :
-    forall sendings from to content cont gen actors,
-      sendings >< ([:: Build_actor from (send to content cont) gen] \cup actors)
-        ~(Send from to content)~>
-        ([:: Build_sending to from content] \cup sendings) >< ([:: Build_actor from cont gen] \cup actors)
+    forall sendings from to content cont gen actors s s' a a',
+      perm_eq s sendings ->
+      perm_eq s' ([:: Build_sending to from content] \cup sendings) ->
+      perm_eq_actors a ([:: Build_actor from (send to content cont) gen] \cup actors) ->
+      perm_eq_actors a' ([:: Build_actor from cont gen] \cup actors) ->
+      s >< a ~(Send from to content)~> s' >< a'
 (* new transition *)
 | trans_new :
-    forall sendings parent behv cont gen actors,
-      sendings >< ([:: Build_actor parent (new behv cont) gen] \cup actors)
-        ~(New parent (generated gen parent))~>
-        sendings ><
-                  ([:: Build_actor (generated gen parent) (become behv) 0
-                   ; Build_actor parent (cont (generated gen parent)) (S gen)
-                  ] \cup actors)
+    forall sendings parent behv cont gen actors s s' a a',
+      perm_eq s sendings ->
+      perm_eq s' sendings ->
+      perm_eq_actors a ([:: Build_actor parent (new behv cont) gen] \cup actors) ->
+      perm_eq_actors a' ([:: Build_actor (generated gen parent) (become behv) 0
+                          ; Build_actor parent (cont (generated gen parent)) (S gen)
+                         ] \cup actors) ->
+      s >< a ~(New parent (generated gen parent))~> s' >< a'
 (* self transition *)
 | trans_self :
-    forall sendings me cont gen actors,
-      sendings >< ([:: Build_actor me (self cont) gen] \cup actors)
-        ~(Self me)~>
-        sendings >< ([:: Build_actor me (cont me) gen] \cup actors)
+    forall sendings me cont gen actors s s' a a',
+      perm_eq s sendings ->
+      perm_eq s' sendings ->
+      perm_eq_actors a ([:: Build_actor me (self cont) gen] \cup actors) ->
+      perm_eq_actors a' ([:: Build_actor me (cont me) gen] \cup actors) ->
+      s >< a ~(Self me)~> s' >< a'
 where "c1 '~(' t ')~>' c2" := (trans t c1 c2).
 
 Hint Constructors trans.
